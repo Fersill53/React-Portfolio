737f2831a61fe643af1ae79b78c1dc94f02bb04c
Fernando Siller <fernando@Fernandos-MBP.attlocal.net>
2023-08-31 17:48:44 -0500

    added file structure, filled in about.js

---
+++ react-portfolio/src/components/About.js
@@ -0,0 +1,10 @@
+function About() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>Hello my name is Fernando Siller, a new but very passionate webe developer with a never ending thirst for learning new things.</p>
+        </div>
+    );
+}
+
+export default About;
